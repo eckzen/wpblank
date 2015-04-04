c027da344f993bf15900381530e68587d3ee9fad
eckzen <eckzen@yahoo.com>
2015-04-05 05:21:36 +0800

    before i change the sass structure

--- footer.php
+++ footer.php
@@ -12,13 +12,17 @@
 		</div>	<!-- END of main-page -->
 	</div> <!-- END  of full -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'blank' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'blank' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'blank' ), 'blank', '<a href="http://raphaeljaybernardo.com" rel="designer">Raphael Jay Bernardo</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<div class="full">
+	<div class="main-page">
+		<footer id="colophon" class="site-footer inner" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'blank' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'blank' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'blank' ), 'blank', '<a href="http://raphaeljaybernardo.com" rel="designer">Raphael Jay Bernardo</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div> <!-- END of  main-page -->
+</div>	<!-- END of full -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
